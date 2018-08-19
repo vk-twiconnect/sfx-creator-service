34b07246fd6de86bbde677cd2240e588748efe2f
Vlad Khazin <vlad.khazin@icssolutions.ca>
2018-08-19 22:28:11 +0000

    Adding env variable for port and testing on Heroku

--- app.js
+++ app.js
@@ -3,28 +3,29 @@ const app = express();
 const zip = require('./zip');
 const port = process.env.PORT || 3000;
 
-const removeDir = (dir) => {
-}
+const packageJson = require('./package.json');
 
 app.use(express.json())
 
 app.get('/', (req, res) => res.send({
-    message: 'Service is running!'
-  }));
+  name: packageJson.anme,
+  version: packageJson.version,
+  description: packageJson.description,
+  author: packageJson.author,
+  nodeVersion: process.version
+}));
 
 app.post('/zip', (req, res) => {
-
-    zip.zip(req.get('x-api-key'), req.body, (err, data) => {
-
-        if(err) {
-            console.error(JSON.stringify(err));
-            res.status(500).send(err.message);
-        } else {
-            zip.remove_dir(data.folder_name)
-            delete data.folder_name
-            res.send(data)
-        }
-    })
+  zip.zip(req.get('x-api-key'), req.body, (err, data) => {
+    if(err) {
+        console.error(JSON.stringify(err));
+        res.status(500).send(err.message);
+    } else {
+        zip.remove_dir(data.folder_name)
+        delete data.folder_name
+        res.send(data)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Service listening on port ${port}!`))
