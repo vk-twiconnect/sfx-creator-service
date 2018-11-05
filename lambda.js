48dab5aad8a62fd49a1c7f14abd268642042e47a
dashkevych <nibbler135@gmail.com>
2018-11-05 07:37:33 +0200

    integration done

---
+++ lambda.js
@@ -0,0 +1,17 @@
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+const binaryMimeTypes = [
+  'application/octet-stream',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'application/zip'
+]
+const server = awsServerlessExpress
+  .createServer(app, null, binaryMimeTypes)
+exports.handler = (event, context) =>
+  awsServerlessExpress.proxy(server, event, context
+)
