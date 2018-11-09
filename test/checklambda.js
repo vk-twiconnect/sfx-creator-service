144e50f28d49d0e56ed746a92e39d0023d5591d5
= <nibbler135@gmail.com>
2018-11-09 21:00:09 +0200

    Update test/checklambda.js

--- test/checklambda.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-
-describe('lambda handler', function() {
-    it('handler shouldn`t be undefined', function(done) {
-        const handler = require('../lambda')
-        assert.notEqual(handler.handler,undefined)
-        done()
-    })
-})
